f537ceec1465b8feb1f6124345a28b74421a0a60
hundredvisionsguy <cwinikka@gmail.com>
2020-10-05 21:37:36 -0700

    initializing project v1.0

---
+++ pdfmerger.py
@@ -0,0 +1,28 @@
+# pdfmerger.py
+from pathlib import Path
+from PyPDF2 import PdfFileMerger
+
+# Set the base path to this current directory
+BASE_PATH = (
+    Path.cwd()
+    / "input"
+)
+
+# Add a BASE_PATH / "filename.pdf" for each page you want to merge
+# Make sure you put them in the correct order
+pdf_paths = [BASE_PATH / "TestDoc01.pdf",
+             BASE_PATH / "TestDoc02.pdf",
+             BASE_PATH / "TestDoc03.pdf"]
+
+if __name__ == "__main__":
+    # create a merger object
+    pdf_merger = PdfFileMerger()
+    
+    # append files in pdf_paths
+    for path in pdf_paths:
+        pdf_merger.append(str(path))
+
+    # write merged contents to file
+    output_path = Path.cwd() / "output" / "YourMergedDocument.pdf"
+    with output_path.open(mode="wb") as output_file:
+        pdf_merger.write(output_file)
